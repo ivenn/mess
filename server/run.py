2c55f4599c9770bd09464337560b5b4ff18e7b25
Aleksei Bokov <abokov.nnov@gmail.com>
2017-08-30 22:27:44 -0700

    Refactor server function to classes

--- server/run.py
+++ server/run.py
@@ -1,16 +1,19 @@
+from server.mess_server import MessServer
 from server.models import session, init_db
-from server.loop import run_server
 from server.models.user import User
 from server.config import configure_logging
+
+
+def add_users(db_session):
+    for name, passwd in [('userA', 'passA'),
+                         ('userB', 'passB')]:
+        if not db_session.query(User).filter(User.name == name).all():
+            db_session.add(User(name, passwd))
+    db_session.commit()
 
 
 if __name__ == '__main__':
     configure_logging()
     init_db()
-    s = session()
-    for name, passwd in [('userA', 'passA'),
-                         ('userB', 'passB')]:
-        if not s.query(User).filter(User.name == name).all():
-            s.add(User(name, passwd))
-    s.commit()
-    run_server()
+    add_users(session())
+    MessServer().run()
