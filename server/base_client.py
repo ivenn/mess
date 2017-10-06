349fb566263f62a2164b56c8178f0688c2c81eef
Aleksei Bokov <abokov.nnov@gmail.com>
2017-10-05 22:13:39 -0700

    Split Client to 2 classes and move dispatch logic to Client

---
+++ server/base_client.py
@@ -0,0 +1,104 @@
+import logging
+
+from server.online import ONLINE_USERS
+from server.models.user import User
+from server.messages import Message, NormalMessage, CMD_CHANGE_STATUS
+from server.data_utils import DataBuffer, DataParser
+
+
+log = logging.getLogger(__name__)
+
+
+class ClientIsAlreadyLoggedInException(Exception):
+    pass
+
+
+class NoSuchStatusException(Exception):
+    pass
+
+
+CLIENT_OFFLINE = 'OFFLINE'
+
+USER_STATUS_DEFAULT = 'ONLINE'
+USER_STATUS_BUSY = 'BUSY'
+USER_STATUSES = [USER_STATUS_BUSY,
+                 USER_STATUS_DEFAULT]
+
+
+class BaseClient:
+
+    def __init__(self, conn, server=None):
+        self.conn = conn
+        self.server = server  # just to get debug info about server state
+        self.__user = None
+        self.__status = None
+        self.data_buffer = DataBuffer()
+        self.data_parser = DataParser()
+
+    def __repr__(self):
+        return "Client(%s:%s)" % (self.conn.getpeername()[0], self.conn.getpeername()[1])
+
+    @property
+    def info(self):
+        return None
+
+    @property
+    def user(self):
+        return self.__user
+
+    @user.setter
+    def user(self, user):
+        if isinstance(user, User):
+            if self.__user:
+                raise ClientIsAlreadyLoggedInException(self.__user)
+            else:
+                self.__user = user
+                ONLINE_USERS[user.name] = self
+                self.status = USER_STATUS_DEFAULT
+        elif user is None:
+            del ONLINE_USERS[self.user.name]
+            self.status = None
+            self.__user = None
+        else:
+            raise ValueError(user)
+
+    @property
+    def status(self):
+        return self.__status if self.user else self.user
+
+    @status.setter
+    def status(self, new_status):
+        if new_status in USER_STATUSES or new_status is None:
+            self.__status = new_status
+            status_msg = new_status if new_status else CLIENT_OFFLINE
+            for friend_name in [u.name for u in self.user.all_friends if u.name in ONLINE_USERS]:
+                client = ONLINE_USERS[friend_name]
+                client.send(NormalMessage(cmd=CMD_CHANGE_STATUS, params=[self.user.name, status_msg]))
+        else:
+            raise NoSuchStatusException(new_status)
+
+    def handle(self, msg):
+        raise NotImplementedError()
+
+    def recv(self, data):
+        """
+        Receive bytes and handle them
+        """
+        log.info("{self} got: {data}".format(self=self, data=data))
+        raw_messages = self.data_buffer.push(data)
+        # if we got complete message lets handle it
+        for raw_msg in raw_messages:
+            msg = self.data_parser.parse(raw_msg)
+            self.handle(msg)
+
+    def send(self, msg):
+        """
+        Sends Message or bytes to client
+        @param msg: Message subclass instance or bytes or string
+        """
+        if type(msg) is str:
+            msg = msg.encode('utf-8')
+        if isinstance(msg, Message):
+            msg = msg.as_bytes()
+        self.conn.send(msg)
+        log.info("{client} <<< {msg}".format(client=self, msg=msg))
--- server/client.py
+++ server/client.py
@@ -2,19 +2,16 @@ import logging
 from functools import wraps
 
 from server.models import session
-from server.online import ONLINE_USERS
 from server.models.user import User
-from server.messages import DataBuffer, DataParser, Message
-from server.messages import CMD_CHANGE_STATUS, CMD_INFO
-from server.messages import NormalMessage, PayloadMessage
-from server.dispatcher import MessageDispatcher
+from server.online import ONLINE_USERS
+
+from server.base_client import BaseClient, CLIENT_OFFLINE, ClientIsAlreadyLoggedInException
+
+from server.messages import CMD_INFO, CMD_LOGIN, CMD_LOGOUT, CMD_FRIENDS, CMD_MESSAGE
+from server.messages import PayloadMessage
 
 
-log = logging.getLogger('client')
-
-
-class NoSuchStatusException(Exception):
-    pass
+log = logging.getLogger(__name__)
 
 
 class NoSuchUserException(Exception):
@@ -45,14 +42,8 @@ class ClientIsNotLoggedInException(Exception):
     pass
 
 
-class ClientIsAlreadyLoggedIn(Exception):
+class NoHandlerForCmdRegisteredException(Exception):
     pass
-
-CLIENT_OFFLINE = 'OFFLINE'
-
-USER_STATUS_DEFAULT = 'ONLINE'
-USER_STATUS_BUSY = 'BUSY'
-USER_STATUSES = [USER_STATUS_BUSY, USER_STATUS_DEFAULT]
 
 
 def login_required(func):
@@ -68,88 +59,47 @@ def login_required(func):
     return wrapped
 
 
-class Client:
+def cmd_handler_cls(cls):
 
-    def __init__(self, conn, server=None):
-        self.conn = conn
-        self.server = server  # just to get debug info about server state
-        self.__user = None
-        self.__status = None
-        self.data_buffer = DataBuffer()
-        self.data_parser = DataParser()
-        self.msg_dispatcher = MessageDispatcher()
+    cls.handlers = {}
+    for methodname in dir(cls):
+        method = getattr(cls, methodname)
+        if hasattr(method, '_registered_cmd'):
+            cls.handlers.update({method._registered_cmd: methodname})
 
-    def __repr__(self):
-        return "Client(%s:%s)" % (self.conn.getpeername()[0], self.conn.getpeername()[1])
+    return cls
 
-    @property
-    def info(self):
-        return None
 
-    @property
-    def user(self):
-        return self.__user
+def register_cmd(cmd):
 
-    @user.setter
-    def user(self, user):
-        if isinstance(user, User):
-            if self.__user:
-                raise ClientIsAlreadyLoggedIn(self.__user)
-            else:
-                self.__user = user
-                ONLINE_USERS[user.name] = self
-                self.status = USER_STATUS_DEFAULT
-        elif user is None:
-            del ONLINE_USERS[self.user.name]
-            self.status = None
-            self.__user = None
-        else:
-            raise ValueError(user)
+    def wrapper(func):
+        func._registered_cmd = cmd
+        return func
 
-    @property
-    def status(self):
-        return self.__status if self.user else self.user
+    return wrapper
 
-    @status.setter
-    def status(self, new_status):
-        if new_status in USER_STATUSES or new_status is None:
-            self.__status = new_status
-            status_msg = new_status if new_status else CLIENT_OFFLINE
-            for friend_name in [u.name for u in self.user.all_friends if u.name in ONLINE_USERS]:
-                client = ONLINE_USERS[friend_name]
-                client.send(NormalMessage(cmd=CMD_CHANGE_STATUS, params=[self.user.name, status_msg]))
-        else:
-            raise NoSuchStatusException(new_status)
 
-    def recv(self, data):
-        """
-        Receive bytes and handle them
-        """
-        log.info("{self} got: {data}".format(self=self, data=data))
-        raw_messages = self.data_buffer.push(data)
-        # if we got complete message lets handle it
-        for raw_msg in raw_messages:
-            msg = self.data_parser.parse(raw_msg)
-            self.msg_dispatcher.dispatch(self, msg)
+@cmd_handler_cls
+class Client(BaseClient):
 
-    def send(self, msg):
-        """
-        Sends Message or bytes to client
-        @param msg: Message subclass instance or bytes or string
-        """
-        if type(msg) is str:
-            msg = msg.encode('utf-8')
-        if isinstance(msg, Message):
-            msg = msg.as_bytes()
-        self.conn.send(msg)
-        log.info("{client} <<< {msg}".format(client=self, msg=msg))
+    def handle(self, msg):
+        try:
+            msg_handler_func = getattr(self, self.handlers[msg.cmd])
+        except KeyError:
+            raise NoHandlerForCmdRegisteredException(msg.cmd)
+        return msg_handler_func(msg)
 
-    def login_as(self, name, password):
+    @register_cmd(CMD_LOGIN)
+    def login_as(self, msg):
         """
         Login client
         """
+        if len(msg.params) != 2:
+            raise InvalidUserCredentials()
+        else:
+            name, password = msg.params
         if self.user:
-            raise ClientIsAlreadyLoggedIn(self.user)
+            raise ClientIsAlreadyLoggedInException(self.user)
         user = session().query(User).filter(User.name == name).one()
         if user:
             if user.name in ONLINE_USERS:
@@ -163,19 +113,22 @@ class Client:
         else:
             raise InvalidUserCredentials()
 
+    @register_cmd(CMD_LOGOUT)
     @login_required
-    def logout(self):
+    def logout(self, msg=None):
         """
         Logout client
         """
         log.info("{user} logged out from {client}".format(user=self.user, client=self))
         self.user = None
 
+    @register_cmd(CMD_FRIENDS)
     @login_required
-    def get_friends(self, online_only=False):
+    def get_friends(self, msg):
         """
         Returns dict {friend's name: info}
         """
+        online_only = False  # TODO: to message params
         res = {}
         for friend in self.user.all_friends:
             if friend.name in ONLINE_USERS:
@@ -184,10 +137,10 @@ class Client:
                 res[friend.name] = CLIENT_OFFLINE
         self.send(PayloadMessage(CMD_INFO, [], res))
 
+    @register_cmd(CMD_MESSAGE)
     @login_required
     def send_message_to(self, msg):
         # verify msg
-        assert isinstance(msg, PayloadMessage)
         to = msg.params[0]
         # verify to
         user_to = session().query(User).filter(User.name == to).one()
